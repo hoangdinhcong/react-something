9608195bf226f693c0931b114489574457ca6709
hoangdinhcong <hodico.iam@gmail.com>
2019-07-18 11:11:41 +0700

    learning

---
+++ src/components/counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = { count: 0 };
+
+  handleIncresment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncresment}
+          className="btn btn-secondary btn-sm"
+        >
+          Incresment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
