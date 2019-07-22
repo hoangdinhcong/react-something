1bdbf6656788aad05adfa83c38486c91d59c571e
hoangdinhcong <hodico.iam@gmail.com>
2019-07-22 23:13:05 +0700

    counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0 };
-
-  handleIncresment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncresment}
+          onClick={() => this.props.onIncresement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Incresment
         </button>
-      </React.Fragment>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
